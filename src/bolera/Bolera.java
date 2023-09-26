523620b4f60a1297ce05aba032619acb3a43d422
Nacho Garcia <ignaciogarcia.dev@gmail.com>
2023-09-27 01:51:53 +0200

    proyecto maquina cobrar tickets

---
+++ src/bolera/Bolera.java
@@ -0,0 +1,23 @@
+
+package bolera;
+
+
+public class Bolera {
+
+    
+    public static void main(String[] args) {
+        int option=0;
+        
+        switch(option){
+            case 1:
+                break;
+                
+            case 2:
+                break;
+                
+            case 3:
+                break;
+        }
+    }
+    
+}
